ff1ecb3839953ea44204ea6f7af6fc93714ec178
Ruben MoyaB <l19141174@queretaro.tecnm.mx>
2021-08-27 18:04:39 -0500

    Creacion de Repositorio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabajos_Unidad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
